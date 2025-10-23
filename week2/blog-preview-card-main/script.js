7163041a56be7b7ebe6e0edd6af38bfc3f556c90
pattcaroline <43247066+pattcaroline@users.noreply.github.com>
2025-10-23 10:21:14 -0400

    Add files via upload
    Final fixes on CSS and added JS mouseover effect.

---
+++ week2/blog-preview-card-main/script.js
@@ -0,0 +1,16 @@
+const active = document.getElementById('active');
+
+active.addEventListener('click', function() {
+    active.style.color = '#f4d04eff';
+    
+}) 
+
+active.addEventListener('mouseover', function() {
+    active.style.color = '#f4d04eff';
+    
+}) 
+
+active.addEventListener('mouseout', function() {
+    active.style.color = 'hsl(0, 0%, 7%)';
+    
+}) 
