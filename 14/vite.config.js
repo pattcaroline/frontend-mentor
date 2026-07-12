6c3d0622cdf63fb5b480b02283480349a483a2f2
pattcaroline <43247066+pattcaroline@users.noreply.github.com>
2026-07-12 04:34:05 -0400

    Add files via upload
    folder path updated

--- 14/vite.config.js
+++ 14/vite.config.js
@@ -3,5 +3,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/frontend-mentor/14/tip-calculator-app-main/dist/",
+  base: "/frontend-mentor/14/dist/",
 });
